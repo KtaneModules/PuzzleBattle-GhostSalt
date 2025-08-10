bafd93f82e0a9cf928112753fd41021535349325
GhostSalt <mhanna756@c2ken.net>
2025-08-10 22:35:35 +0100

    -Initial commit

---
+++ Assets/PuzzleBattleScript.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using KModkit;
+using Rnd = UnityEngine.Random;
+
+public class PuzzleBattleScript : MonoBehaviour
+{
+    static int _moduleIdCounter = 1;
+    int _moduleID = 0;
+
+    public KMBombModule Module;
+    public KMBombInfo Bomb;
+    public KMAudio Audio;
+    public KMSelectable[] CardSelectables;
+    public KMSelectable SubmitButton;
+    public GameObject[] Highlights;
+    public SpriteRenderer SubmitButtonRend;
+    public SpriteRenderer[] CardRends;
+    public Sprite[] AllCardBacks;
+    public Sprite[] AllSubmitButtonSprites;
+    public GameObject[] Fingers;
+    public SpriteRenderer[] SpeechBubbles;
+    public Sprite[] AllSpeechBubbleSprites;
+    public GameObject FindTheSpade;
+
+    private Puzzle Puzzle;
+    private int SelectionIx = -1;
+    private List<float> CardInitXs = new List<float>();
+    private bool CannotPress = true;
+    private bool Suspense;
+    private static readonly string[] Ordinals = new[] { "first", "second", "third", "fourth" };
+
+    private Sprite FindCardBack(int id)
+    {
+        foreach (var back in AllCardBacks)
+            if (back.name == "card sheet " + id)
+                return back;
+        return null;
+    }
+
+    void Awake()
+    {
+        _moduleID = _moduleIdCounter++;
+
+        var puzzleGenerator = new PuzzleGenerator();
+        Puzzle = puzzleGenerator.Puzzle;
+
+        Debug.LogFormat("[Puzzle Battle #{0}] Selected rules:\n{1}", _moduleID, Puzzle.Rules.Select((x, ix) => (ix + 1) + ". " + x.Name).Join("\n"));
+        Debug.LogFormat("[Puzzle Battle #{0}] The cards are as follows: {1}.", _moduleID, Puzzle.Cards.Join(", "));
+        Debug.LogFormat("[Puzzle Battle #{0}] Select the {1} card.", _moduleID, Ordinals[Array.IndexOf(Puzzle.Cards, Suit.Spade)]);
+
+        for (int i = 0; i < CardSelectables.Length; i++)
+        {
+            int x = i;
+            CardSelectables[x].OnInteract += delegate { if (!CannotPress) CardSelect(x); return false; };
+            CardSelectables[x].OnHighlight += delegate { Highlights[x].SetActive(true); };
+            CardSelectables[x].OnHighlightEnded += delegate { Highlights[x].SetActive(false); };
+            Highlights[x].SetActive(false);
+            CardRends[x].sprite = FindCardBack(Puzzle.Rules[x].ID);
+            Fingers[x].SetActive(false);
+            SpeechBubbles[x].gameObject.SetActive(false);
+            CardInitXs.Add(CardSelectables[x].transform.localPosition.x);
+        }
+
+        SubmitButton.OnInteract += delegate { if (!CannotPress) SubmitButtonPress(); return false; };
+        SubmitButton.OnHighlight += delegate { if (!Suspense) SubmitButtonRend.sprite = AllSubmitButtonSprites[1]; };
+        SubmitButton.OnHighlightEnded += delegate { if (!Suspense) SubmitButtonRend.sprite = AllSubmitButtonSprites[0]; };
+
+        for (int i = 0; i < CardSelectables.Length; i++)
+            CardSelectables[i].transform.localPosition = new Vector3(CardInitXs.Last(), CardSelectables[i].transform.localPosition.y, CardSelectables[i].transform.localPosition.z);
+
+        Module.OnActivate += delegate { StartCoroutine(IntroAnim()); };
+    }
+
+    void Start()
+    {
+        SubmitButton.transform.localScale = Vector3.zero;
+    }
+
+    private void CardSelect(int pos)
+    {
+        CardSelectables[pos].AddInteractionPunch();
+        Audio.PlaySoundAtTransform("select", CardSelectables[pos].transform);
+        for (int i = 0; i < Fingers.Length; i++)
+            Fingers[i].SetActive(false);
+        if (SelectionIx != pos)
+        {
+            SelectionIx = pos;
+            Fingers[pos].SetActive(true);
+            SubmitButton.transform.localScale = Vector3.one;
+        }
+        else
+        {
+            SelectionIx = -1;
+            SubmitButton.transform.localScale = Vector3.zero;
+        }
+    }
+
+    private void SubmitButtonPress()
+    {
+        SubmitButton.AddInteractionPunch();
+        CannotPress = true;
+        Suspense = true;
+        SubmitButtonRend.sprite = AllSubmitButtonSprites[2];
+        StartCoroutine(HandleSubmit());
+    }
+
+    private IEnumerator HandleSubmit()
+    {
+        var correct = SelectionIx == Array.IndexOf(Puzzle.Cards, Suit.Spade);
+        if (correct)
+            Debug.LogFormat("[Puzzle Battle #{0}] You selected the {1} card, which was correct. Module solved!", _moduleID, Ordinals[SelectionIx]);
+        else
+            Debug.LogFormat("[Puzzle Battle #{0}] You selected the {1} card, which was incorrect. Strike!", _moduleID, Ordinals[SelectionIx]);
+
+        Audio.PlaySoundAtTransform("submit", SubmitButton.transform);
+        yield return new WaitForSeconds(0.1f);
+        SubmitButtonRend.sprite = AllSubmitButtonSprites[0];
+        yield return new WaitForSeconds(0.3f);
+        SubmitButton.transform.localScale = Vector3.zero;
+        yield return new WaitForSeconds(0.6f);
+
+        for (int i = 0; i < Fingers.Length; i++)
+            Fingers[i].SetActive(false);
+        SelectionIx = -1;
+
+        Suspense = false;
+
+        if (correct)
+        {
+            Module.HandlePass();
+            Audio.PlaySoundAtTransform("solve", transform);
+            FindTheSpade.SetActive(false);
+            yield return "solve";
+
+            yield return new WaitForSeconds(0.25f);
+            for (int i = 0; i < SpeechBubbles.Length; i++)
+            {
+                yield return new WaitForSeconds(0.5f);
+                SpeechBubbles[i].sprite = AllSpeechBubbleSprites[(int)Puzzle.Cards[i]];
+                SpeechBubbles[i].gameObject.SetActive(true);
+                Audio.PlaySoundAtTransform("speech bubble", SpeechBubbles[i].transform);
+            }
+        }
+        else
+        {
+            Module.HandleStrike();
+            Audio.PlaySoundAtTransform("strike", transform);
+            yield return "strike";
+            CannotPress = false;
+        }
+    }
+
+    private IEnumerator IntroAnim(float duration = 0.25f)
+    {
+        yield return new WaitForSeconds(0.1f);
+        StartCoroutine(PlayDealSounds());
+        float timer = 0;
+        while (timer < duration)
+        {
+            yield return null;
+            timer += Time.deltaTime;
+            CardSelectables[0].transform.localPosition = new Vector3(Mathf.Lerp(CardInitXs.Last(), CardInitXs[0], timer / duration), CardSelectables[0].transform.localPosition.y, CardSelectables[0].transform.localPosition.z);
+            CardSelectables[1].transform.localPosition = new Vector3(Mathf.Lerp(CardInitXs.Last(), CardInitXs[1], Mathf.Clamp01(timer / (duration * (2f / 3)))), CardSelectables[1].transform.localPosition.y, CardSelectables[1].transform.localPosition.z);
+            CardSelectables[2].transform.localPosition = new Vector3(Mathf.Lerp(CardInitXs.Last(), CardInitXs[2], Mathf.Clamp01(timer / (duration * (1f / 3)))), CardSelectables[2].transform.localPosition.y, CardSelectables[2].transform.localPosition.z);
+        }
+
+        CardSelectables[0].transform.localPosition = new Vector3(CardInitXs[0], CardSelectables[0].transform.localPosition.y, CardSelectables[0].transform.localPosition.z);
+        CardSelectables[1].transform.localPosition = new Vector3(CardInitXs[1], CardSelectables[1].transform.localPosition.y, CardSelectables[1].transform.localPosition.z);
+        CardSelectables[2].transform.localPosition = new Vector3(CardInitXs[2], CardSelectables[2].transform.localPosition.y, CardSelectables[2].transform.localPosition.z);
+
+        CannotPress = false;
+    }
+
+    private IEnumerator PlayDealSounds()
+    {
+        Audio.PlaySoundAtTransform("deal", CardSelectables[2].transform);
+        yield return new WaitForSeconds(0.1f);
+        Audio.PlaySoundAtTransform("deal", CardSelectables[1].transform);
+        yield return new WaitForSeconds(0.1f);
+        Audio.PlaySoundAtTransform("deal", CardSelectables[0].transform);
+    }
+
+#pragma warning disable 414
+    private string TwitchHelpMessage = "Use '!{0} 3s' to select the third card, then press \"OK!\".";
+#pragma warning restore 414
+
+    IEnumerator ProcessTwitchCommand(string command)
+    {
+        command = command.ToLowerInvariant();
+        string validcmds = "1234s";
+
+        for (int i = 0; i < command.Length; i++)
+        {
+            if (!validcmds.Contains(command[i]))
+            {
+                yield return "sendtochaterror Invalid command.";
+                yield break;
+            }
+        }
+        yield return null;
+
+        for (int i = 0; i < command.Length; i++)
+        {
+            if (!CannotPress && !Suspense)
+            {
+                if (command[i] == 's')
+                    SubmitButton.OnInteract();
+                else
+                    CardSelectables[int.Parse(command[i].ToString()) - 1].OnInteract();
+                yield return new WaitForSeconds(0.2f);
+            }
+        }
+    }
+    IEnumerator TwitchHandleForcedSolve()
+    {
+        var answer = Array.IndexOf(Puzzle.Cards, Suit.Spade);
+        if (SelectionIx != answer)
+            CardSelectables[answer].OnInteract();
+        yield return null;
+        SubmitButton.OnInteract();
+    }
+}
---
+++ Assets/PuzzleGenerator.cs
@@ -0,0 +1,472 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PuzzleGenerator
+{
+    public Puzzle Puzzle;
+
+    public PuzzleGenerator()
+    {
+        startOver:
+
+        Suit[] allSuits = { Suit.Spade, Suit.Heart, Suit.Club, Suit.Diamond };
+        var allPossibleArrangements =
+        (from a in allSuits
+         from b in allSuits.Where(s => s != a)
+         from c in allSuits.Where(s => s != a && s != b)
+         from d in allSuits.Where(s => s != a && s != b && s != c)
+         select new[] { a, b, c, d }).ToList();
+
+        var chosenArrangement = allPossibleArrangements.PickRandom();
+
+        var allPossibleRules = new List<IRule>()
+            {
+                new HeartNextToClub(),
+                new ClubNextToDiamond(),
+                new DiamondNextToHeart(),
+
+                new HeartNotNextToClub(),
+                new ClubNotNextToDiamond(),
+                new DiamondNotNextToHeart(),
+
+                new HeartThenClub(),
+                new ClubThenDiamond(),
+                new DiamondThenHeart(),
+
+                new HeartThenDiamond(),
+                new ClubThenHeart(),
+                new DiamondThenClub(),
+
+                new SpadeNextToHeart(),
+                new SpadeNextToClub(),
+                new SpadeNextToDiamond(),
+
+                new SpadeNotNextToHeart(),
+                new SpadeNotNextToClub(),
+                new SpadeNotNextToDiamond(),
+
+                new EndHeart(),
+                new EndClub(),
+                new EndDiamond(),
+
+                new NotEndHeart(),
+                new NotEndClub(),
+                new NotEndDiamond(),
+
+            }.Where(x => x.Matches(chosenArrangement)).ToList().Shuffle();
+
+        var chosenRules = new List<IRule>();
+        var ruleIx = 0;
+
+        while (!IsUniqueSolution(chosenArrangement, chosenRules, allPossibleArrangements))
+        {
+            chosenRules.Add(allPossibleRules[ruleIx]);
+            ruleIx++;
+        }
+
+        for (int i = chosenRules.Count - 1; i >= 0; i--)
+        {
+            IRule ruleToTest = chosenRules[i];
+
+            List<IRule> testRules = new List<IRule>();
+            foreach (IRule r in chosenRules)
+                if (r != ruleToTest)
+                    testRules.Add(r);
+
+            if (IsUniqueSolution(chosenArrangement, testRules, allPossibleArrangements))
+                chosenRules.RemoveAt(i);
+        }
+
+        if (chosenRules.Count() > 4 || chosenRules.Count() < 3)
+            goto startOver;
+
+        if (chosenRules.Count() == 3)
+            chosenRules.Add(new NoRule());
+
+        chosenRules = chosenRules.Shuffle();
+        Puzzle = new Puzzle(chosenArrangement, chosenRules);
+    }
+
+    public static bool IsUniqueSolution(Suit[] solution, List<IRule> rules, List<Suit[]> allArrangements)
+    {
+        var validArrangements = allArrangements.Where(arr => rules.All(rule => rule.Matches(arr))).ToList();
+
+        if (validArrangements.Count != 1)
+            return false;
+
+        for (int i = 0; i < 4; i++)
+            if (validArrangements[0][i] != solution[i])
+                return false;
+        return true;
+    }
+}
+
+public class Puzzle
+{
+    public Suit[] Cards;
+    public List<IRule> Rules;
+
+    public Puzzle(Suit[] cards, List<IRule> rules)
+    {
+        Cards = cards;
+        Rules = rules;
+    }
+}
+
+public enum Suit { Spade, Heart, Club, Diamond };
+
+public interface IRule
+{
+    int ID { get; }
+    string Name { get; }
+    bool Matches(Suit[] cards);
+}
+
+#region Rules A
+
+public class HeartNextToClub : IRule
+{
+    public int ID { get { return 0; } }
+    public string Name { get { return "A heart is next to a club."; } }
+
+    public bool Matches(Suit[] cards)
+    {
+        for (int i = 0; i < cards.Count() - 1; i++)
+            if (cards[i] == Suit.Heart && cards[i + 1] == Suit.Club || cards[i] == Suit.Club && cards[i + 1] == Suit.Heart)
+                return true;
+        return false;
+    }
+}
+
+public class ClubNextToDiamond : IRule
+{
+    public int ID { get { return 1; } }
+    public string Name { get { return "A club is next to a diamond."; } }
+
+    public bool Matches(Suit[] cards)
+    {
+        for (int i = 0; i < cards.Count() - 1; i++)
+            if (cards[i] == Suit.Club && cards[i + 1] == Suit.Diamond || cards[i] == Suit.Diamond && cards[i + 1] == Suit.Club)
+                return true;
+        return false;
+    }
+}
+
+public class DiamondNextToHeart : IRule
+{
+    public int ID { get { return 2; } }
+    public string Name { get { return "A diamond is next to a heart."; } }
+
+    public bool Matches(Suit[] cards)
+    {
+        for (int i = 0; i < cards.Count() - 1; i++)
+            if (cards[i] == Suit.Diamond && cards[i + 1] == Suit.Heart || cards[i] == Suit.Heart && cards[i + 1] == Suit.Diamond)
+                return true;
+        return false;
+    }
+}
+
+#endregion
+#region Rules B
+
+public class HeartNotNextToClub : IRule
+{
+    public int ID { get { return 3; } }
+    public string Name { get { return "A heart is not next to a club."; } }
+
+    public bool Matches(Suit[] cards)
+    {
+        for (int i = 0; i < cards.Count() - 1; i++)
+            if (cards[i] == Suit.Heart && cards[i + 1] == Suit.Club || cards[i] == Suit.Club && cards[i + 1] == Suit.Heart)
+                return false;
+        return true;
+    }
+}
+
+public class ClubNotNextToDiamond : IRule
+{
+    public int ID { get { return 4; } }
+    public string Name { get { return "A club is not next to a diamond."; } }
+
+    public bool Matches(Suit[] cards)
+    {
+        for (int i = 0; i < cards.Count() - 1; i++)
+            if (cards[i] == Suit.Club && cards[i + 1] == Suit.Diamond || cards[i] == Suit.Diamond && cards[i + 1] == Suit.Club)
+                return false;
+        return true;
+    }
+}
+
+public class DiamondNotNextToHeart : IRule
+{
+    public int ID { get { return 5; } }
+    public string Name { get { return "A diamond is not next to a heart."; } }
+
+    public bool Matches(Suit[] cards)
+    {
+        for (int i = 0; i < cards.Count() - 1; i++)
+            if (cards[i] == Suit.Diamond && cards[i + 1] == Suit.Heart || cards[i] == Suit.Heart && cards[i + 1] == Suit.Diamond)
+                return false;
+        return true;
+    }
+}
+
+#endregion
+#region Rules C
+
+public class HeartThenClub : IRule
+{
+    public int ID { get { return 6; } }
+    public string Name { get { return "A heart is directly to the left of a club."; } }
+
+    public bool Matches(Suit[] cards)
+    {
+        for (int i = 0; i < cards.Count() - 1; i++)
+            if (cards[i] == Suit.Heart && cards[i + 1] == Suit.Club)
+                return true;
+        return false;
+    }
+}
+
+public class ClubThenDiamond : IRule
+{
+    public int ID { get { return 7; } }
+    public string Name { get { return "A club is directly to the left of a diamond."; } }
+
+    public bool Matches(Suit[] cards)
+    {
+        for (int i = 0; i < cards.Count() - 1; i++)
+            if (cards[i] == Suit.Club && cards[i + 1] == Suit.Diamond)
+                return true;
+        return false;
+    }
+}
+
+public class DiamondThenHeart : IRule
+{
+    public int ID { get { return 8; } }
+    public string Name { get { return "A diamond is directly to the left of a heart."; } }
+
+    public bool Matches(Suit[] cards)
+    {
+        for (int i = 0; i < cards.Count() - 1; i++)
+            if (cards[i] == Suit.Diamond && cards[i + 1] == Suit.Heart)
+                return true;
+        return false;
+    }
+}
+
+public class HeartThenDiamond : IRule
+{
+    public int ID { get { return 9; } }
+    public string Name { get { return "A heart is directly to the left of a diamond."; } }
+
+    public bool Matches(Suit[] cards)
+    {
+        for (int i = 0; i < cards.Count() - 1; i++)
+            if (cards[i] == Suit.Heart && cards[i + 1] == Suit.Diamond)
+                return true;
+        return false;
+    }
+}
+
+public class ClubThenHeart : IRule
+{
+    public int ID { get { return 10; } }
+    public string Name { get { return "A club is directly to the left of a heart."; } }
+
+    public bool Matches(Suit[] cards)
+    {
+        for (int i = 0; i < cards.Count() - 1; i++)
+            if (cards[i] == Suit.Club && cards[i + 1] == Suit.Heart)
+                return true;
+        return false;
+    }
+}
+
+public class DiamondThenClub : IRule
+{
+    public int ID { get { return 11; } }
+    public string Name { get { return "A diamond is directly to the left of a club."; } }
+
+    public bool Matches(Suit[] cards)
+    {
+        for (int i = 0; i < cards.Count() - 1; i++)
+            if (cards[i] == Suit.Diamond && cards[i + 1] == Suit.Club)
+                return true;
+        return false;
+    }
+}
+
+#endregion
+#region Rules D
+
+public class SpadeNextToHeart : IRule
+{
+    public int ID { get { return 12; } }
+    public string Name { get { return "A spade is next to a heart."; } }
+
+    public bool Matches(Suit[] cards)
+    {
+        for (int i = 0; i < cards.Count() - 1; i++)
+            if (cards[i] == Suit.Spade && cards[i + 1] == Suit.Heart || cards[i] == Suit.Heart && cards[i + 1] == Suit.Spade)
+                return true;
+        return false;
+    }
+}
+
+public class SpadeNextToClub : IRule
+{
+    public int ID { get { return 13; } }
+    public string Name { get { return "A spade is next to a club."; } }
+
+    public bool Matches(Suit[] cards)
+    {
+        for (int i = 0; i < cards.Count() - 1; i++)
+            if (cards[i] == Suit.Spade && cards[i + 1] == Suit.Club || cards[i] == Suit.Club && cards[i + 1] == Suit.Spade)
+                return true;
+        return false;
+    }
+}
+
+public class SpadeNextToDiamond : IRule
+{
+    public int ID { get { return 14; } }
+    public string Name { get { return "A spade is next to a diamond."; } }
+
+    public bool Matches(Suit[] cards)
+    {
+        for (int i = 0; i < cards.Count() - 1; i++)
+            if (cards[i] == Suit.Spade && cards[i + 1] == Suit.Diamond || cards[i] == Suit.Diamond && cards[i + 1] == Suit.Spade)
+                return true;
+        return false;
+    }
+}
+#endregion
+#region Rules E
+
+public class SpadeNotNextToHeart : IRule
+{
+    public int ID { get { return 15; } }
+    public string Name { get { return "A spade is not next to a heart."; } }
+
+    public bool Matches(Suit[] cards)
+    {
+        for (int i = 0; i < cards.Count() - 1; i++)
+            if (cards[i] == Suit.Spade && cards[i + 1] == Suit.Heart || cards[i] == Suit.Heart && cards[i + 1] == Suit.Spade)
+                return false;
+        return true;
+    }
+}
+
+public class SpadeNotNextToClub : IRule
+{
+    public int ID { get { return 16; } }
+    public string Name { get { return "A spade is not next to a club."; } }
+
+    public bool Matches(Suit[] cards)
+    {
+        for (int i = 0; i < cards.Count() - 1; i++)
+            if (cards[i] == Suit.Spade && cards[i + 1] == Suit.Club || cards[i] == Suit.Club && cards[i + 1] == Suit.Spade)
+                return false;
+        return true;
+    }
+}
+
+public class SpadeNotNextToDiamond : IRule
+{
+    public int ID { get { return 17; } }
+    public string Name { get { return "A spade is not next to a diamond."; } }
+
+    public bool Matches(Suit[] cards)
+    {
+        for (int i = 0; i < cards.Count() - 1; i++)
+            if (cards[i] == Suit.Spade && cards[i + 1] == Suit.Diamond || cards[i] == Suit.Diamond && cards[i + 1] == Suit.Spade)
+                return false;
+        return true;
+    }
+}
+
+#endregion
+#region Rules F
+
+public class EndHeart : IRule
+{
+    public int ID { get { return 18; } }
+    public string Name { get { return "One of the cards on the far left or far right is a heart."; } }
+
+    public bool Matches(Suit[] cards)
+    {
+        return cards.First() == Suit.Heart || cards.Last() == Suit.Heart;
+    }
+}
+
+public class EndClub : IRule
+{
+    public int ID { get { return 19; } }
+    public string Name { get { return "One of the cards on the far left or far right is a club."; } }
+
+    public bool Matches(Suit[] cards)
+    {
+        return cards.First() == Suit.Club || cards.Last() == Suit.Club;
+    }
+}
+
+public class EndDiamond : IRule
+{
+    public int ID { get { return 20; } }
+    public string Name { get { return "One of the cards on the far left or far right is a diamond."; } }
+
+    public bool Matches(Suit[] cards)
+    {
+        return cards.First() == Suit.Diamond || cards.Last() == Suit.Diamond;
+    }
+}
+
+#endregion
+#region Rules G
+
+public class NotEndHeart : IRule
+{
+    public int ID { get { return 21; } }
+    public string Name { get { return "None of the cards on the far left or far right are hearts."; } }
+
+    public bool Matches(Suit[] cards)
+    {
+        return cards.First() != Suit.Heart && cards.Last() != Suit.Heart;
+    }
+}
+
+public class NotEndClub : IRule
+{
+    public int ID { get { return 22; } }
+    public string Name { get { return "None of the cards on the far left or far right are clubs."; } }
+
+    public bool Matches(Suit[] cards)
+    {
+        return cards.First() != Suit.Club && cards.Last() != Suit.Club;
+    }
+}
+
+public class NotEndDiamond : IRule
+{
+    public int ID { get { return 23; } }
+    public string Name { get { return "None of the cards on the far left or far right are diamonds."; } }
+
+    public bool Matches(Suit[] cards)
+    {
+        return cards.First() != Suit.Diamond && cards.Last() != Suit.Diamond;
+    }
+}
+
+#endregion
+
+public class NoRule : IRule
+{
+    public int ID { get { return 999; } }
+    public string Name { get { return "Always applies."; } }
+
+    public bool Matches(Suit[] cards) { return true; }
+}
